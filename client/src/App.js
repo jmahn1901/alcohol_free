34e0a8e0c03994a743529a3be8c6b85ca39f08d2
swon95 <shimtkddnjs13@gmail.com>
2022-11-01 20:24:26 +0900

    home,header,footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
+import Header from './Header';
+import Footer from './Footer'
 
 function App() {
   return (
+    <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blog" element={<Blog />} />
       </Routes>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,22 @@
 const Home = () => {
     return (
-        <div>
-            더미 홈
+        <><section id="hero" class="hero d-flex align-items-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4">
+              <h2 data-aos="fade-up" class="aos-init aos-animate">Focus On What Matters</h2>
+              <blockquote data-aos="fade-up" data-aos-delay="100" class="aos-init aos-animate">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis cum recusandae eum laboriosam voluptatem repudiandae odio, vel exercitationem officiis provident minima. </p>
+              </blockquote>
+              <div class="d-flex aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+                <a href="#about" class="btn-get-started">Get Started</a>
+                <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+              </div>
+    
+            </div>
+          </div>
         </div>
+      </section></>
     )
 }
 
